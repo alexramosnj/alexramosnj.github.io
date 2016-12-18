ec90d632b98a06faf9ec9d2e76cf6102018a5447
alexramosnj <alexramosnj@gmail.com>
2016-12-18 17:14:01 -0600

    ANM Timesheet Demo
    Initial commit

---
+++ scripts/ANM_Timesheet.js
@@ -0,0 +1,42 @@
+﻿/*
+    File name: ANM_Timesheet
+    Author: Ramos Jesús
+*/
+
+
+/* Controls' behavior*/
+
+$(document).ready(function () {
+
+    $("#BtnAddRow").click(function () {
+        $('#TblTimesheet').append('<tr name="TrTimesheetRow"><td><input type="checkbox" name="ChkTimesheetSelectedRow" /></td><td><input type="text" name="TxtTimesheetProjectNameRow" /></td><td><input type="text" name="ChkTimesheetWorkDescriptionRow" /></td><td><input type="number" min="0.5" step="0.5" name="ChkTimesheetHoursWorkedRow" /></td></tr>');
+    });
+
+    $("#BtnRemoveRows").click(function () {
+        $('input:checkbox[name=ChkTimesheetSelectedRow]:checked').each(function () {
+            $(this).closest('tr').remove();
+        });
+    });
+
+    $("#BtnCancelTimesheet").click(function () {
+        $("#TblTimesheet").children(':not(thead)').remove();
+        $('.PersonalData').each(function () {
+            $(this).val("");
+        });
+    });
+
+    $("#BtnSubmitTimesheet").click(function () {
+        alert('BtnSubmitTimesheet');
+    });
+
+    $("#BtnSubmitTimesheet").click(function () {
+        alert('BtnSubmitTimesheet');
+    });
+
+    $('input:text').each(function () {
+        $(this).closest('tr').remove();
+    });
+
+});
+
+
